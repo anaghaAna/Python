87c14b20dfb33676c187f819e97384f4ada951d7
anaghaAna <130470941+anaghaAna@users.noreply.github.com>
2023-04-12 18:34:50 +0530

    Add files via upload

---
+++ anagha/nested.py
@@ -0,0 +1,10 @@
+x=[]
+even=[]
+for i in range(1,100):
+    x.append(i)
+print(i)
+for i in x:
+    if i%2==0:
+        even.append(i)
+print(even)
+        
---
+++ anagha/newfile.py
@@ -0,0 +1,8 @@
+x=input("Enter ur name")
+i = 1
+while i<=5:
+    print(x)
+    i+=1
+    
+
+    
